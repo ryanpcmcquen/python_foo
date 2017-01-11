7f8c6d60b24a72bf25832259af08cf76df3f326e
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2017-01-11 19:57:01 +0000

    Page 169.
    Signed-off-by: Ryan P.C. McQuen <ryanpcmcquen@gmail.com>

---
+++ greeter_2.py
@@ -0,0 +1,7 @@
+## `username` is a parameter.
+def greet_user(username):
+    """Display a simple greeting."""
+    print("Hello, " + username.title() + "!")
+
+## `'jesse'` is an argument.
+greet_user('jesse')
---
+++ pets.py
@@ -0,0 +1,16 @@
+pets = [
+    'dog',
+    'cat',
+    'dog',
+    'goldfish',
+    'cat',
+    'rabbit',
+    'cat'
+]
+
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
