f968ec8edafd878b7989e4ee80f65624617ac55a
Ryan McQuen <ryanpcmcquen@member.fsf.org>
2017-02-08 17:14:44 +0000

    Page 175.
    Signed-off-by: Ryan McQuen <ryanpcmcquen@member.fsf.org>

--- pets_2.py
+++ pets_2.py
@@ -1,6 +1,6 @@
-def describe_pet(animal_type, pet_name):
+def describe_pet(pet_name, animal_type = 'cat'):
     """Display information about a pet."""
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
-describe_pet('cat', 'shadow')
+describe_pet(pet_name = 'snowball', animal_type = 'cat')
